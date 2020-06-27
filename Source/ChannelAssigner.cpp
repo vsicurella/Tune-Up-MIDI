1e6d3f17747e01585cc72a8cd8ae118e245bcac4
Vincenzo Sicurella <sicurella12@gmail.com>
2020-06-27 00:15:50 -0400

    Improvements to ChannelAssigner (still needs work)

--- Source/ChannelAssigner.cpp
+++ Source/ChannelAssigner.cpp
@@ -10,17 +10,51 @@
 
 #include "ChannelAssigner.h"
 
-TuneUpMidiChannelAssigner::TuneUpMidiChannelAssigner(MPEInstrument* mpeInstIn)
+TuneUpMidiChannelAssigner::TuneUpMidiChannelAssigner(MPEInstrument* mpeInstIn, Array<int>& notesOnIn)
+	: notesOn(notesOnIn)
 {
     mpeInst = mpeInstIn;
 
 	pitchbendOfChannel.resize(16);
 	pitchbendOfChannel.fill(8192);
+
+	noteToChannelIndex.resize(128);
+	noteToChannelIndex.fill(-1);
 }
 
 TuneUpMidiChannelAssigner::~TuneUpMidiChannelAssigner()
 {
     
+}
+
+int TuneUpMidiChannelAssigner::findNextFreeChannel() const
+{
+	int freeChannel = -1;
+
+	if (numNotesOn <= voiceLimit)
+	{
+		if (roundRobinMode)
+		{
+			do
+			{
+				freeChannel = (channelsOn[channelsOn.size() - 1] + 1) % 16;
+			} while (channelsOn.contains(freeChannel) && !channelsToSkip.contains(freeChannel));
+		}
+
+		else
+		{
+			for (int i = 0; i < 16; i++)
+			{
+				if (!channelsOn.contains(i) && !channelsToSkip.contains(i))
+				{
+					freeChannel = i;
+					break;
+				}
+			}
+		}
+	}
+
+	return freeChannel;
 }
 
 int TuneUpMidiChannelAssigner::getFreeOrExistingChannel(int noteIn, int pitchbendIn) const
@@ -33,80 +67,71 @@ int TuneUpMidiChannelAssigner::getFreeOrExistingChannel(int noteIn, int pitchben
 
 	if (!oneChannelPerNote)
 	{
-		int channelOut = -1;
-
-		for (auto n : notesOn)
+		for (auto ch : channelsOn)
 		{
-			int ch = n.key;
-			pitchbend = pitchbendOfChannel[ch];
-
-			if (pitchbendIn == pitchbend)
-			{
-				channelOut = ch;
-			}
-
-			if (channelOut == ch && n.value.key == noteIn)
-			{
-				isSameNote = true;
-				break;
-			}
-		}
-
-		return channelOut + 1;
-	}
-
-	for (int i = 0; i < 16; i++)
-	{
-		if (!channelsOn.contains(i))
-		{
-			return i;
+			int pb = pitchbendOfChannel[ch];
+			if (pb == pitchbendIn)
+				return ch;
 		}
 	}
 	
-    return -1;
+	return findNextFreeChannel();
 }
 
 int TuneUpMidiChannelAssigner::noteOn(int noteIn, int pitchbendIn)
 {
-	int channel = 0;
+	int channel = -1;
 	if (mpeInst && mpeInst->isMemberChannel(channel) || !mpeInst)
 	{
 		channel = getFreeOrExistingChannel(noteIn, pitchbendIn);
+		
+		if (!notesOn.contains(noteIn))
+		{
+			notesOn.add(noteIn);
+			numNotesOn = notesOn.size();
 
-		ChannelMidiPitch pitch = ChannelMidiPitch(channel, MidiPitch(noteIn, pitchbendIn));
-		notesOn.addIfNotAlreadyThere(pitch);
-		channelsOn.addIfNotAlreadyThere(channel);
-		pitchbendOfChannel.set(channel, pitchbendIn);
+			channelsOn.addUsingDefaultSort(channel);
+			pitchbendOfChannel.set(channel, pitchbendIn);
+
+			noteToChannelIndex.set(noteIn, channelsOn.indexOf(channel));
+		}
 	}
 
-	numNotesOn = notesOn.size();
-	return channel + 1;
+	return channel;
 }
 
-int TuneUpMidiChannelAssigner::noteOff(int noteIn, int pitchbendIn)
+int TuneUpMidiChannelAssigner::noteOff(int noteIn)
 {
-	int channel = -1;
-
+	int note, channel;
+	// Improve
 	for (int i = 0; i < notesOn.size(); i++)
 	{
-		ChannelMidiPitch note = notesOn[i];
+		note = notesOn[i];
+		channel = channelsOn[noteToChannelIndex[note]];
 
-		if (note.value.key == noteIn && note.value.value == pitchbendIn)
+		if (note == noteIn)
 		{
-			channel = note.key;
 			notesOn.remove(i);
+			noteToChannelIndex.set(note, -1);
+			numNotesOn = notesOn.size();
 
-			// turn off channel if no other notes on it
-			bool channelHasOtherNotes = false;
-			for (auto n : notesOn)
+			bool removeChannel = true;
+			if (!oneChannelPerNote)
 			{
-				if (n.key == channel)
-					channelHasOtherNotes = true;
+				// retuan channel if has other notes on it
+				for (auto chInd : noteToChannelIndex)
+				{
+					if (channel == channelsOn[chInd])
+					{
+						removeChannel = false;
+						break;
+					}
+				}
 			}
 
-			if (!channelHasOtherNotes)
+			if (removeChannel)
 			{
-				channelsOn.removeAllInstancesOf(channel);
+				channelsOn.removeFirstMatchingValue(channel);
 				pitchbendOfChannel.set(channel, 8192);
 			}
 
@@ -114,8 +139,7 @@ int TuneUpMidiChannelAssigner::noteOff(int noteIn, int pitchbendIn)
 		}
 	}
 
-	numNotesOn = notesOn.size();
-	return channel + 1;
+	return channel;
 }
 
 void TuneUpMidiChannelAssigner::allNotesOff()
@@ -125,28 +149,19 @@ void TuneUpMidiChannelAssigner::allNotesOff()
 	channelsOn.clear();
 }
 
-int TuneUpMidiChannelAssigner::getChannelOfNote(int noteIn, int pitchbendIn) const
+bool TuneUpMidiChannelAssigner::isChannelFree(int channelIn) const
 {
-	for (auto n : notesOn)
-	{
-		if (n.value.key == noteIn && n.value.value == pitchbendIn)
-		{
-			return n.key + 1;
-		}
-	}
+	return channelsOn.indexOf(channelIn) < 0;
+}
 
-	return -1;
+int TuneUpMidiChannelAssigner::getChannelOfNote(int noteIn) const
+{
+	return noteToChannelIndex[noteIn];
 }
 
 int TuneUpMidiChannelAssigner::getPitchbendOfChannel(int channelIn) const
 {
 	return pitchbendOfChannel[channelIn];
-}
-
-
-Array<ChannelMidiPitch> TuneUpMidiChannelAssigner::getAllNotesOn() const
-{
-	return notesOn;
 }
 
 int TuneUpMidiChannelAssigner::getVoiceLimit() const
@@ -156,7 +171,7 @@ int TuneUpMidiChannelAssigner::getVoiceLimit() const
 
 int TuneUpMidiChannelAssigner::getNumNotesOn() const
 {
-    return getAllNotesOn().size();
+	return numNotesOn;
 }
 
 void TuneUpMidiChannelAssigner::setVoiceLimit(int voiceLimitIn)
--- Source/ChannelAssigner.h
+++ Source/ChannelAssigner.h
@@ -7,6 +7,7 @@
  
     This class is used to separate MIDI Data so that there is only a single
     MIDI note per channel, within the provided MPEInstrument.
+
     The advantage of this over the JUCE MPEChannelAssigner is that this explicitly only
     allows one note per channel plus notes are differentiated not just by MIDI note number
     but also pitchbend data. This allows the triggering of the same MIDI note with different
@@ -25,21 +26,32 @@ class TuneUpMidiChannelAssigner
 	
 	Array<int> pitchbendOfChannel; // 16 values representing pitchbend per midi channel, 8192 meaning pitchbend at unity
     
-    int voiceLimit = -1;
+    int voiceLimit = INT_MAX;
     int numNotesOn = 0;
-	
-	Array<ChannelMidiPitch> notesOn;
+
+	Array<MidiPitch> pitchesOn;
+	Array<int>& notesOn; // Tuning degrees, not necessarily MIDI notes
 	Array<int> channelsOn;
+
+	Array<int> noteToChannelIndex; // index is noteOn index, value is channelsOn index
+
+	Array<int> channelsToSkip;
     
 	// allows channels to contain more than one note if pitchbend value is the same
 	// but per-note vibrato and other expression is limited
-	bool oneChannelPerNote = false; 
+	bool oneChannelPerNote = false;
+	bool roundRobinMode = false;
     
     
 public:
     
-    TuneUpMidiChannelAssigner(MPEInstrument* mpeInstIn);
+    TuneUpMidiChannelAssigner(MPEInstrument* mpeInstIn, Array<int>& notesOnIn);
     ~TuneUpMidiChannelAssigner();
+
+	/*
+		Returns midi channel 0 - 15
+	*/
+	int findNextFreeChannel() const;
     
     /*
         Returns an available channel number for a given MidiPitch.
@@ -49,20 +61,28 @@ public:
 	int getFreeOrExistingChannel(int noteIn, int pitchbendIn) const;
 
 	/*
-		Helper function that uses a note on a channel, and returns the channel it was used on
+		Helper function that uses a note on a channel, and returns the channel it was used on.
+
+		Note in is tuning degree, not necessarily midi note
 	*/
 	int noteOn(int noteIn, int pitchbendIn);
     
     /*
         Will return the channel number the MidiPitch exists on.
-     */
-    int noteOff(int noteIn, int pitchbendIn);
 
-	Array<ChannelMidiPitch> getAllNotesOn() const;
+		Note in is tuning degree, not necessarily midi note
+     */
+    int noteOff(int noteIn);
     
     void allNotesOff();
+
+	/*
+		Input channel num 0-15
+	*/
+
+	bool isChannelFree(int channelIn) const;
     
-    int getChannelOfNote(int noteIn, int pitchbendIn) const;
+    int getChannelOfNote(int noteIn) const;
 
 	/*
 		Input channel number 0 - 15 
